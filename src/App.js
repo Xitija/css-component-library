8ec8944d8eb5e170c85d0c98c3c754aa03dbaa53
Kshitija Kadam <kshitijakadam04@gmail.com>
2023-07-29 18:52:39 +0530

    chore : added Page routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import { Menu } from "./components/Menu/Menu";
+import { Home } from "./pages/Home/Home";
+import { Explore } from "./pages/Explore/Explore";
+import { Playlist } from "./pages/Playlist/Playlist";
+import { WatchLater } from "./pages/WatchLater/WatchLater";
+import { Category } from "./pages/Category/Category";
+import { PlayListVideos } from "./pages/PlayListVideos/PlayListVideos";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="menu-container">
+        <Menu />
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/playlists" element={<Playlist />} />
+          <Route path="/watchlater" element={<WatchLater />} />
+          <Route path="/category/:category" element={<Category />} />
+          <Route path="/playlist/:playlistId" element={<PlayListVideos />} />
+        </Routes>
+      </div>
     </div>
   );
 }
